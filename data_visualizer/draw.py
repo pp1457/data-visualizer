4fe22f4242b93185027fd2ae0bfb613260a0a260
Paul Liao <liaoyunyang0815@gmail.com>
2024-08-28 16:44:20 +0800

    initial commit

---
+++ data_visualizer/draw.py
@@ -0,0 +1,86 @@
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+def draw_radar(metrics, scores, chunking_method, output_path):
+    num_vars = len(metrics)
+
+    angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+    scores += scores[:1]
+    angles += angles[:1] 
+
+    fig, ax = plt.subplots(figsize=(12, 12), subplot_kw=dict(polar=True))
+
+    ax.set_theta_zero_location('N')
+    # ax.yaxis.set_visible(False)  # Hide radial grid lines
+    # ax.xaxis.set_visible(False)  # Hide angular grid lines
+    # ax.spines['polar'].set_visible(False)  # Hide the outer frame
+
+
+    ax.set_ylim(0, 1)
+    break_point = [round(i*0.2, 1) for i in range(6)]
+    ax.set_yticks(break_point)
+    ax.set_yticklabels(break_point)  # You can choose to label the y-axis
+
+    ax.set_xticks(angles[:-1])  # Set the x-ticks to the angles, excluding the last one to avoid duplication
+    ax.set_xticklabels(metrics)  # Set the x-tick labels
+
+    line_color = '#1f77b4'  # A strong blue for the line
+    fill_color = '#aec7e8'  # A lighter blue for the fill
+    text_color = '#ff7f0e'  # A contrasting orange for text
+    scatter_color = '#2ca02c'  # A vibrant green for the scatter points
+
+    # Draw hexagon
+    ax.plot(angles, scores, linewidth=2, linestyle="solid", color=line_color, label="Metrics")
+    ax.fill(angles, scores, color=fill_color, alpha=0.25)
+
+    for ti, di in zip(angles, scores):
+        ax.text(ti, di-0.15, round(di, 4), color=text_color, ha='center', va='center')
+
+    ax.scatter(angles, scores, color=scatter_color, s=50)
+
+    # Add a title
+    ax.set_title(f"{chunking_method} Radar Graph")
+
+
+    plt.savefig(output_path)
+    # plt.show()
+
+def draw_box(chunking_methods, scores, metric, output_path):
+
+    data = []
+
+    for index, method in enumerate(chunking_methods):
+        for question_id, score in enumerate(scores[index]):
+            data.append([method, question_id, score])
+
+    df = pd.DataFrame(data, columns=["chunking_method", "question_id", "score"])
+
+    # Create the box plot
+    plt.figure(figsize=(12, 40))
+    sns.boxplot(x="chunking_method", y="score", data=df)
+    plt.title(f"{metric} of Different Chunking Methods")
+    plt.xlabel("Chunking Method")
+    plt.ylabel("Score")
+    plt.xticks(rotation=45)
+    plt.savefig(output_path)
+
+def main():
+    # metrics = ["Hit Rate", "MAP", "MRR", "NDCG", "TNR"]
+    # scores = [0.3, 0.5, 0.99999, 0.1, 0.2]
+    # draw(metrics, scores)
+
+    methods = ["by_markdown", "by_page", "semantic"]
+    scores = []
+    scores.append([0.1, 0.3, 0.2])
+    scores.append([0.4, 0.3, 0.1])
+    scores.append([0.2, 0.2, 0.5])
+
+    draw_box(methods, scores, "hit_rate")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ data_visualizer/visualize.py
@@ -0,0 +1,79 @@
+import json
+import os
+import pathlib
+from data_visualizer.draw import draw_radar, draw_box
+
+def read_json_to_dict(file_path):
+    """Read JSON data from a file and return it as a dictionary."""
+
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"The file {file_path} does not exist.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON from file {file_path}: {e}")
+        return None
+
+def visualize(json_files, result_dir):
+    metrics = ["hit_rate", "map", "mrr", "ndcg", "tnr"]
+    chunking_methods = []
+    
+    method_metric_score = {}
+
+    for json_path in json_files:
+
+        data = read_json_to_dict(json_path)
+
+        scores = [ data[metric]/100 for metric in metrics]
+
+        filename = os.path.splitext(os.path.basename(data["search_filename"]))[0]
+        method = str(data["search_chunking_method"])
+        embedding_model = str(data["search_embedding_model"])
+
+        if not method in chunking_methods:
+            chunking_methods.append(method)
+
+        method_metric_score[method] = {}
+        k_value = data["k_value"]
+        threshold = data["threshold"]
+
+        for metric in metrics:
+            method_metric_score[method][metric] = data[f"{metric}_list"]
+
+        output_dir = pathlib.Path(f"{result_dir}/{filename}/{embedding_model}/k={k_value}&threshold={threshold}/")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_path = output_dir / method
+        draw_radar(metrics, scores, method, output_path)
+
+    for metric in metrics:
+        scores = []
+        for method in chunking_methods:
+            scores.append(method_metric_score[method][metric])
+        output_dir = pathlib.Path(f"{result_dir}/{filename}/{embedding_model}/k={k_value}&threshold={threshold}/")
+        output_dir.mkdir(parents=True, exist_ok=True)
+        output_path = output_dir / metric
+        draw_box(chunking_methods, scores, metric, output_path)
+
+
+        
+
+def main():
+    visualize(
+        [
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=by_markdown_number_of_hash=4.json",
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=by_page.json",
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=recursive_char_chunk_size=1000_overlap_size=100.json",
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=semantic_model=mxbai-embed-large_buffer_size=3_chunk_number=60.json",
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=semantic_model=text-embedding-3-small_buffer_size=3_chunk_number=60.json",
+        "/Users/liaoyunyang/Downloads/fubon_k=3,th=0.5.json/k=3@threshold=0.5@methods=semantic_model=text-embedding-ada-002_buffer_size=3_chunk_number=60.json"
+        ],
+        "data/result"
+    )
+
+if __name__ == "__main__":
+    main()
+        
+
